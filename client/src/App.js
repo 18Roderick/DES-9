ee03596b581ab573109bfdd8c88d64035d2c08fe
Roderick Romero <rjrr507@gmail.com>
2020-05-24 19:28:40 -0500

    cambios

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import Merch from './components/Merch/index';
 import News from './components/News/index';
 import Header from './components/Hero/Hero';
-import NavBar from './components/NavBar/NavBar'
-
-import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 import { BrowserRouter as Router } from 'react-router-dom';
+
 function App() {
   return (
     <div className="">
-      <header className=""></header>
-
       <div className="container-fluid"></div>
       <Header></Header>
       <NavBar></NavBar>
