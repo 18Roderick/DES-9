3556d1ecb60ee26872382bcb0d628ccaddb70b58
emilio sucre <emilio.sucre@utp.ac.pa>
2020-05-24 19:42:44 -0500

    Cambios

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import Header from './components/Hero/Hero';
 
-import NavBar from './components/Navbar/NavBar';
+import NavBar from './components/NavBar/NavBar';
 
 import Switcher from './components/Switch'
 
