ef6bcdef253e19b68dd96c276cd23720f55049ae
emilio sucre <emilio.sucre@utp.ac.pa>
2020-06-01 21:13:17 -0500

    Funciona

--- client/src/utils/Helpers.js
+++
@@ -1,7 +0,0 @@
-export const utils ={
-  truncateText: function truncateText(text, length = 10) {
-    this.text = text;
-    if (text.length > length) return `${text.slice(0, length)}`;
-    return text;
-  }
-}
