cde4669981c5655849907ee13eaec5c8b16c9d98
emilio sucre <emilio.sucre@utp.ac.pa>
2020-06-01 21:23:31 -0500

    Cambios en el App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import Merch from './components/Merch/index';
-import News from './components/News/index';
+import { BrowserRouter as Router } from 'react-router-dom';
+//
 import Header from './components/Hero/Hero';
+ 
 import NavBar from './components/NavBar/NavBar';
+ 
+import Switcher from './components/Switch';
 
-import './App.css';
-
+ 
 function App() {
   return (
-    <div className="">
-      <header className=""></header>
-
-      <div className="container-fluid"></div>
-      {/* <Header></Header> */}
+    <Router>
+      <Header></Header>
       <NavBar></NavBar>
-      <News></News>
-      <Merch></Merch>
- 
-    </div>
+      <Switcher></Switcher>
+    </Router>
   );
 }
-
+ 
 export default App;
