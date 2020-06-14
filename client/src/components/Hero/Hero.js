9e6f4a3fbb8fb947d02c17e7b4fab3ddbd50f743
emilio sucre <emilio.sucre@utp.ac.pa>
2020-06-14 16:12:16 -0500

    Se refactorizo el navbar y se implemente el switch router

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import herobanner from './../../assets/img/GreenLogo-Hero.png';
+import React, { Fragment } from "react";
+import herobanner from "./../../assets/img/GreenLogo-Hero.png";
+import NavBarPrueba from "../NavBarPrueba/NavBarPrueba";
 
-
-
-const Hero = () =>(
-
-    <div className="site-header">
+function Hero() {
+  return (
+    <Fragment>
+        <div className="site-header">
           <div className="redes-sociales">
-          <a href="#" className=""><i className="fab fa-facebook-f"></i></a>
-          <a href="#" className=""><i className="fab fa-twitter"></i></a>
-          <a href="#" className=""><i className="fab fa-pinterest"></i></a>
-          <a href="#" className=""><i className="fab fa-youtube"></i></a>
-          <a href="#" className=""><i className="fab fa-instagram"></i></a>
+            <a href="#" className="">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" className="">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="#" className="">
+              <i className="fab fa-pinterest"></i>
+            </a>
+            <a href="#" className="">
+              <i className="fab fa-youtube"></i>
+            </a>
+            <a href="#" className="">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </div>
+          <img className="imagenhero img-fluid" src={herobanner} />
         </div>
-      <img className = 'imagenhero img-fluid' src = {herobanner}/>        
-
-  </div>
-
-
-);
+    </Fragment>
+  );
+}
 
 export default Hero;
