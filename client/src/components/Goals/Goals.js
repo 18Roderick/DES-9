a37df0b2a2f33b22d6c990cc39e7a45e45db94f4
emilio sucre <emilio.sucre@utp.ac.pa>
2020-07-13 22:56:35 -0500

    Componente Metas

--- client/src/components/Goals/Goals.js
+++ client/src/components/Goals/Goals.js
@@ -4,39 +4,37 @@ import SingleGoals from './SingleGoals/SingleGoals'
 const datas = [
     {
       id: 1,
-      image: "Producto",
-      cardData: {
-        name: 'Producto',
-        type: 'Producto',
-        description: 'Producto',
-        info:
-          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-      },
-      price: '300.00',
+      mImagen: "Producto",
+      mTitulo: 'Producto',
+      mDescripcion:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but',
+      Meta:{
+        mActual: 0.00,
+        mMeta: 350.00,
+        }
     },
     {
       id: 2,
-      image: "loli2",
-      cardData: {
-        name: 'Producto',
-        type: 'Producto',
-        description: 'Producto',
-        info:
-          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-      },
-      price: '750.00',
+      mImagen: "Producto",
+      mTitulo: 'Producto',
+      mDescripcion:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but',
+      Meta:{
+        mActual: 500.00,
+        mMeta: 800.00,
+      }
+          
     },
     {
       id: 3,
-      image: "loli2",
-      cardData: {
-        name: 'Producto',
-        type: 'Producto',
-        description: 'Producto',
-        info:
-          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-      },
-      price: '200.00',
+      mImagen: "Producto",
+      mTitulo: 'Producto',
+      mDescripcion:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but',
+      Meta:{
+        mActual: 500.00,
+        mMeta: 500.00,
+        }
     },
   ];
 
--- client/src/components/Goals/SingleGoals/SingleGoals.js
+++ client/src/components/Goals/SingleGoals/SingleGoals.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import GoalBar from './GoalBar/GoalBar';
+import sello from '../../../assets/img/GreenLogo-Hero2.png';
+import FondoMeta from '../../../assets/img/FondoMetas.jpg';
 
-const SingleGoals = ({ mTitulo, mDescripcion, mImagen, mMeta }) => {
+const SingleGoals = ({ data: { mImagen, mTitulo, mDescripcion, Meta } }) => {
+
+  var styles = {
+    'background-image':'url()',
+  }
   return (
-    <div className=" col-md-6 col-sm-12 col-xl-4">
-      <div className="singleGoals">
-        <h2 className="metasTitulo">{mTitulo}</h2>
-        <p className="metasDescripcion">{mDescripcion}</p>
-        <GoalBar />
-      </div>
+    <div className=" col-md-6 col-sm-12 col-xl-4 contenedorMetas">     
+    
+        <div className=" singleGoals">
+        {/* <img src={FondoMeta} className='fondoMeta'/>    */}
+          <div className="sgContenedor">
+            <div className="metaTituloSello">
+              <h2 className="metasTitulo">{mTitulo}</h2>
+              <div className=" sMeta sellosRecompensas">
+                <img src={sello} />
+              </div>
+            </div>
+
+            <p className="metasDescripcion">{mDescripcion}</p>
+          </div>
+
+          <div className="sgBarraProgreso">
+            {/* <h4>Progreso</h4> */}
+            <div className="barraMeta">
+              <GoalBar Meta={Meta} />
+            </div>
+
+            <button className="boton btnMetas btn btn-primary">Ver Meta</button>
+          </div>
+        </div>
     </div>
   );
 };
