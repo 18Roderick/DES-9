c04902f5bc31c5a3702b5f718e999bb2dc82ccb3
Roderick Romero <rjrr507@gmail.com>
2020-07-28 17:27:31 -0500

    endpoint usuario

--- server/controllers/usuario/index.js
+++ server/controllers/usuario/index.js
@@ -1,26 +1,34 @@
-const {Usuario} = require('../../models')
+const { Usuario } = require('../../models');
 
+module.exports.getUsuario = async (req, res) => {
+  try {
+    let user = req.user;
 
-module.exports.getUsuario = async (req, res) =>{
-    try {
-       let user = req.user;
-       
-       if(user){
-            res.json({
-                success:true,
-                data:user
-            })
-       }else{
-           res.json({
-               success:true,
-               message:'Usuario no Proporcionado',
-               data: []
-           })
-       }
-    } catch (error) {
-        res.status(500).json({
-            success:false,
-            message: 'Intente mas tarde'
-        })
+    if (user.id) {
+      user = await Usuario.findById(user.id, '-rol -activo -password ');
+
+      if (user)
+        res.json({
+          success: true,
+          data: user
+        });
+      else
+        res.json({
+          success: false,
+          mensaje: 'Usuario no encontrado',
+          data: user
+        });
+    } else {
+      res.json({
+        success: true,
+        message: 'Usuario no Proporcionado',
+        data: []
+      });
     }
-}
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Intente mas tarde'
+    });
+  }
+};
