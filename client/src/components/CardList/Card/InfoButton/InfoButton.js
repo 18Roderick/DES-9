71bbf932bc6d9d2814ae946e65cfa695d4d7f19c
emilio sucre <emilio.sucre@utp.ac.pa>
2020-08-02 15:50:03 -0500

    perfilUsuario

--- client/src/components/CardList/Card/InfoButton/InfoButton.js
+++ client/src/components/CardList/Card/InfoButton/InfoButton.js
@@ -27,17 +27,7 @@ function InfoButton({ price }) {
       </div>
 
       <Modal size="xl" show={show} onHide={handleClose} scrollable={true}>
-        <Modal.Header closeButton>{/* <Modal.Title>Modal heading</Modal.Title> */}</Modal.Header>
-        {/* <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-        */}
+        <Modal.Header closeButton></Modal.Header>
         <Modal.Body>
           <ModalPrueba />
         </Modal.Body>
