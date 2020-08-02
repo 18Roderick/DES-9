c237b30307e074d6565c71566dd358e3b52ffc1c
Roderick Romero <rjrr507@gmail.com>
2020-08-02 12:33:25 -0500

    producto carga

--- server/public/js/productos.js
+++ server/public/js/productos.js
@@ -1,3 +1,7 @@
-function productoModal(data){
-    console.log(data)
+function productoModal(data){   
+    const producto = $(data).data('producto');
+    console.log(producto)
+
+    $('selector').html('');
+    $('id demodal').modal('show');
 }
