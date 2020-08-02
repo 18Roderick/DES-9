6a805811afd05e91ca63a4ead8f6c960bc278074
emilio sucre <emilio.sucre@utp.ac.pa>
2020-08-02 16:00:20 -0500

    cambios

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React, { Fragment } from 'react';
 import Switcher from './components/Switch';
 import NavBarPrueba from './components/NavBarPrueba/NavBarPrueba';
 //import Noticias from './components/Noticias/index';
+import Contacto from './components/Contact/index';
 
 function App() {
   return (
     <Fragment>
       <NavBarPrueba/>   
       <Switcher></Switcher>
+      <Contacto></Contacto>
     </Fragment>
   );
 }
